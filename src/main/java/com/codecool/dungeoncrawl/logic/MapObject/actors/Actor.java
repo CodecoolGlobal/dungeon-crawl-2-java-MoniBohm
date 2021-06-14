5bc61ba675f5eadcb339b7ee5e7a9c7180badbc3
NorbertRuff <ruffnorbert88@gmail.com>
2021-06-14 18:48:04 +0200

    [add] basic neighbour cell logic

--- src/main/java/com/codecool/dungeoncrawl/logic/MapObject/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/MapObject/actors/Actor.java
@@ -3,6 +3,7 @@ package com.codecool.dungeoncrawl.logic.MapObject.actors;
 import com.codecool.dungeoncrawl.logic.CellType;
 import com.codecool.dungeoncrawl.logic.Drawable;
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.MapObject.items.Item;
 
 public abstract class Actor implements Drawable {
     private Cell cell;
@@ -15,10 +16,36 @@ public abstract class Actor implements Drawable {
 
     public void initMove(int dx, int dy) { // emptyre mehet csak - item/enemy vizsgálat
         Cell nextCell = cell.getNeighbor(dx, dy);
-        if (isEmptyCell(nextCell)) {
+        validateCell(nextCell);
+    }
+
+    private void validateCell(Cell nextCell) {
+        if(isEmptyCell(nextCell) && isEnemyCell(nextCell)){
+            fightEnemy();
+        }else if (isEmptyCell(nextCell) && isItemCell(nextCell)){
+            pickupItem();
+        }else if (isEmptyCell(nextCell)) {
             move(nextCell);
         }
     }
+
+    private boolean isEnemyCell(Cell nextCell){
+        Actor actor = nextCell.getActor();
+        if (actor instanceof Skeleton) {
+            return true;
+        }
+            return false;
+    }
+
+    private void fightEnemy(){
+        
+    }
+
+    private boolean isItemCell(Cell nextCell){
+        return nextCell.getItem() != null;
+    }
+
+    private void pickupItem(){}
 
     private void move(Cell nextCell) {
         cell.setActor(null);
@@ -27,7 +54,9 @@ public abstract class Actor implements Drawable {
     }
 
     private boolean isEmptyCell(Cell nextCell) {
-        return nextCell.getType() == CellType.FLOOR;
+        return nextCell.getType() == CellType.FLOOR
+                && nextCell.getActor()==null
+                && nextCell.getItem() == null;
     }
 
     public int getHealth() {
