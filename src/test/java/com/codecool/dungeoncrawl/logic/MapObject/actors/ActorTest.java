b4c17773e927fe1c31829caae38868a9ca092902
MoniBohm <monic.bohm@gmail.com>
2021-06-17 15:37:21 +0200

    [test] new Actor test have been implemented

--- src/test/java/com/codecool/dungeoncrawl/logic/MapObject/actors/ActorTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/MapObject/actors/ActorTest.java
@@ -1,5 +1,6 @@
 package com.codecool.dungeoncrawl.logic.MapObject.actors;
 
+import com.codecool.dungeoncrawl.logic.Cell;
 import com.codecool.dungeoncrawl.logic.CellType;
 import com.codecool.dungeoncrawl.logic.GameMap;
 import org.junit.Ignore;
@@ -11,48 +12,75 @@ import static org.junit.jupiter.api.Assertions.*;
 class ActorTest {
     GameMap gameMap = new GameMap(3, 3, CellType.FLOOR);
     Player testPlayer;
+    Enemy testEnemy;
+    Cell testCell;
 
     @BeforeEach
     void setUp() {
-        Player tesPlayer = new Player(gameMap.getCell(1, 1));
+        this.testPlayer = new Player(gameMap.getCell(1, 1));
+        this.testEnemy = new Bucket(gameMap.getCell(2, 2));
     }
 
-    @Ignore
-    void getHealth() {
+    @Test
+    void playerHealthIsOne() {
         testPlayer.health = 1;
         int excepted = 1;
         assertEquals(excepted, testPlayer.getHealth());
     }
 
     @Test
-    void getDamage() {
+    void playerHealthIsZero() {
+        testPlayer.health = 0;
+        int excepted = 0;
+        assertEquals(excepted, testPlayer.getHealth());
     }
 
     @Test
-    void isActorDead() {
+    void playerHealthIsMinus() {
+        testPlayer.health = -1;
+        int excepted = -1;
+        assertEquals(excepted, testPlayer.getHealth());
     }
+
+
+    @Test
+    void playerDamageIsMinus() {
+        testPlayer.damage = -1;
+        int excepted = -1;
+        assertEquals(excepted, testPlayer.getHealth());
+    }
+
+    @Test
+    void givenPlayerHealthIsNull_ThenDead() {
+        testPlayer.health = 0;
+        boolean excepted = true;
+        int actorHealth = testPlayer.health;
+        assertEquals(excepted, testPlayer.isActorDead(actorHealth));
+    }
+
+
+    @Test
+    void givenPlayerHealthIsNull_ThenAlive() {
+        testPlayer.health = 1;
+        boolean excepted = false;
+        int actorHealth = testPlayer.health;
+        assertEquals(excepted, testPlayer.isActorDead(actorHealth));
+    }
+
+//    (enemy.getHealth()) - player.damage;
 
     @Test
     void hitEnemy() {
+        testEnemy.health = 1;
+        testPlayer.damage = 10;
+        assertEquals(-9, testPlayer.hitEnemy(testPlayer, testEnemy));
     }
 
     @Test
     void hitPlayer() {
+        testPlayer.health = 10;
+        testPlayer.damage = 10;
+        assertEquals(0, testPlayer.hitPlayer(testPlayer, testEnemy));
     }
 
-    @Test
-    void fightToTheDeath() {
-    }
-
-    @Test
-    void getCell() {
-    }
-
-    @Test
-    void getX() {
-    }
-
-    @Test
-    void getY() {
-    }
 }
