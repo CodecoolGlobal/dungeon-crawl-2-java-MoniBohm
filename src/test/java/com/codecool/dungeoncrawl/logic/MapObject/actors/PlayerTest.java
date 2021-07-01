dfa363bd7ddb2dd4e9c1b9d4fd377790a2da38b2
MoniBohm <monic.bohm@gmail.com>
2021-07-01 15:13:35 +0200

    [refactor] serialize_andDeserialized_ReturnSameResult test

--- src/test/java/com/codecool/dungeoncrawl/logic/MapObject/actors/PlayerTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/MapObject/actors/PlayerTest.java
@@ -9,30 +9,24 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
-    private List<Item> testInventory;
     private Player testPlayer;
     private Item testCoin;
     private Item testKey;
     private GameMap gameMap = new GameMap(3, 3, CellType.FLOOR);
 
-
     @BeforeEach
     void setUp() {
         this.testPlayer = new Player(gameMap.getCell(1, 1), "name");
-//        this.testInventory = new ArrayList<>();
         this.testCoin = new Coin(gameMap.getCell(2, 2));
         this.testKey = new Key(gameMap.getCell(0, 0));
         testPlayer.putItemToInventory(testCoin);
         testPlayer.putItemToInventory(testKey);
-
     }
 
     @Test
